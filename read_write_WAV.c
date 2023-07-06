fb6eec23dc13855a72bd794c6638a980f7e02f63
TomasNagy <tomi.nagy@outlook.com>
2023-07-06 19:05:40 +0200

    feat: get input from CL

--- read_write_WAV.c
+++ read_write_WAV.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv)
+#define MAX_ARGV_LENGTH 100
+
+int main(int argc, char **argv)
 {
+    if (argc != 4)
+    {
+        printf ("Usage: <program> <input> <output> <factor>\n");
+        return EXIT_FAILURE;
+    }
+
+    char input[MAX_ARGV_LENGTH] = { "" };
+    char output[MAX_ARGV_LENGTH] = { "" };
+    char factor[MAX_ARGV_LENGTH] = { "" };
+
+    strcpy_s (input, MAX_ARGV_LENGTH, argv[1]);
+    strcpy_s (output, MAX_ARGV_LENGTH, argv[2]);
+    strcpy_s (factor, MAX_ARGV_LENGTH, argv[3]);
+
     return EXIT_SUCCESS;
 }
